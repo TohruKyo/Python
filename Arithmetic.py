6bccf6dae82c07a49116d806fe87883ef8f56084
Shruti <shrukdas@gmail.com>
2021-07-09 15:40:47 +0530

    My new Commit !

---
+++ Arithmetic.py
@@ -0,0 +1,20 @@
+x=int(input("What is the first number?"))
+y=int(input("What is the second number?"))
+z=x+y
+print("The sum is",z)
+z=x*y
+print("The multiplication is",z)
+z=x/y
+print("The division is",z)
+z=x%y
+print("The modulus is",z)
+print("The next operation is square root")
+a=int(input("Enter the number whose square root is required"))
+import math
+b=math.sqrt(a)
+print(b)
+z=math.pow(x,y)
+print("The power is",z)
+print("The next operation is floor and ceil respectively")
+print(math.floor(b))
+print(math.ceil(b))
